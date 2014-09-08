3a422be9b5dc0b579afa2923ca115a97a46121da
Zoltan Olah <zol@percolatestudio.com>
2014-09-08 15:08:55 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Meteor wrapper package for [velocity.js](http://julian.com/research/velocity/) version 1.0.0.
+
+Velocity and it's ui.pack are included unmodified and will add themselves to `window` and `$` respectively.
+
---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  summary: "Wrapper for velocity.js includes ui pack (upstream version: 1.0.0)",
+  version: "1.0.0",
+  git: "https://github.com/percolatestudio/meteor-velocityjs.git",
+  upstreamVersion: "1.0.0"
+});
+
+Package.onUse(function(api) {
+  if (api.versionsFrom)
+    api.versionsFrom('METEOR@0.9.1');
+
+  api.addFiles(['velocity.js', 'velocity.ui.js'], 'client');
+});
